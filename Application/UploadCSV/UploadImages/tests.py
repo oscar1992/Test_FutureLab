d288228a22c788fc2c6e53606362bace6025a3c4
oscar1992 <xirious@hotmail.com>
2025-04-23 15:55:03 -0500

    Fourth commit
    Added a minimal tests cases

--- Application/UploadCSV/UploadImages/tests.py
+++ Application/UploadCSV/UploadImages/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
-
+from .models import File, UploadedImage, User
 # Create your tests here.
+
+class FileModelTest(TestCase):
+    def testFile(self):
+        self.user = User.objects.create(username='testuser', email='email@test.com', password='password')
+        self.file = File.objects.create(
+            user=self.user,
+            fileName='testfile.csv',
+            filePath='Uploads/testfile.csv',
+            status='pending'
+        )
+        self.uploaded_image = UploadedImage.objects.create(
+            file=self.file,
+            name='testimage',
+            image='testimage.jpg',
+            description='Test image description',
+            image_path='Uploads/testimage.jpg',
+            status='pending'
+        )
+        try:
+            self.file.save()            
+        except Exception as e:
+            print(f"Error saving file: {e}")
+            self.file.status = 'failed'
+
+        self.assertEqual(self.file.status, 'pending')
+
+class UploadedImageModelTest(TestCase):
+        def testImage(self):
+            self.user = User.objects.create(username='testuser', email='email@test.com', password='password')
+            self.file = File.objects.create(
+                user=self.user,
+                fileName='testfile.csv',
+                filePath='Uploads/testfile.csv',
+                status='pending'
+            )
+            self.uploaded_image = UploadedImage.objects.create(
+                file=self.file,
+                name='testimage',
+                image='testimage.jpg',
+                description='Test image description',
+                image_path='Uploads/testimage.jpg',
+                status='pending'
+            )
+            try:
+                self.uploaded_image.save()            
+            except Exception as e:
+                print(f"Error saving Image: {e}")
+                self.uploaded_image.status = 'failed'
+
+            self.assertEqual(self.file.status, 'pending')
+
+class UserModelTest(TestCase):
+    def testUser(self):
+            self.user = User.objects.create(username='testuser', email='email@test.com', password='password')
+            try:
+                self.user.save()            
+            except Exception as e:
+                print(f"Error saving file: {e}")
+
+            userDB = User.objects.get(username='testuser')
+            self.assertEqual(userDB.username, 'testuser')
